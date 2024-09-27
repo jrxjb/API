44278cccd22c9c43df1b7a6782fe85ffd9306838
jasrock <177959714+jrxjb@users.noreply.github.com>
2024-09-27 00:16:18 -0400

    Se agregó baseView y se definió post para register

--- myapi/myapp/views.py
+++ myapi/myapp/views.py
@@ -1,17 +1,22 @@
-from rest_framework import generics, permissions
+from rest_framework import generics, permissions, status
 from rest_framework.response import Response
 from rest_framework_simplejwt.tokens import RefreshToken 
 from django.contrib.auth.models import User
 from .models import Task
 from .serializers import TaskSerializer,LoginSerializer, RegisterSerializer
+from .baseView import BaseView
 
 
-
-class RegisterView(generics.CreateAPIView):
+class RegisterView(BaseView,generics.CreateAPIView):
     queryset = User.objects.all()
     serializer_class = RegisterSerializer
 
-class LoginView(generics.GenericAPIView):
+    def post(self, request, *args, **kwargs):
+        if request.method != 'POST':
+            return Response({'detail': 'Method not allowed'}, status=status.HTTP_405_METHOD_NOT_ALLOWED)
+        return super().post(request, *args, **kwargs)
+
+class LoginView(BaseView):
     serializer_class = LoginSerializer
 
     def post(self, request, *args, **kwargs):
@@ -24,7 +29,7 @@ class LoginView(generics.GenericAPIView):
             'access': str(refresh.access_token),
         })
 
-class LogoutView(generics.GenericAPIView):
+class LogoutView(BaseView):
     permission_classes = (permissions.IsAuthenticated,)
 
     def post(self, request, *args, **kwargs):
@@ -32,14 +37,14 @@ class LogoutView(generics.GenericAPIView):
             refresh_token = request.data["refresh"]
             token = RefreshToken(refresh_token)
             token.blacklist()
-            return Response(status=204)
+            return Response(status=status.HTTP_204_NO_CONTENT)
         except Exception as e:
-            return Response({"detail": str(e)}, status=400)
+            return Response({"detail": str(e)}, status=status.HTTP_400_BAD_REQUEST)
 
-
-class TaskListCreateView(generics.ListCreateAPIView):
+class TaskListCreateView(BaseView, generics.ListCreateAPIView):
     serializer_class = TaskSerializer
     permission_classes = (permissions.IsAuthenticated,)
+    queryset = Task.objects.all()
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
@@ -47,14 +52,9 @@ class TaskListCreateView(generics.ListCreateAPIView):
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
 
-class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
+class TaskDetailView(BaseView, generics.RetrieveUpdateDestroyAPIView):
     serializer_class = TaskSerializer
     permission_classes = (permissions.IsAuthenticated,)
 
     def get_queryset(self):
         return Task.objects.filter(user=self.request.user)
-
-
-
-
-
